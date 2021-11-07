5361e2847862deb4fd3d77d8aa64081c60ec865a
mafor2 <mattkar@googlemail.com>
2021-11-07 20:59:56 +0100

    add caaba/mecca

---
+++ src/CAABA/mecca/batch_mafor/mafor.bat
@@ -0,0 +1,21 @@
+# -*- Shell-script -*-
+
+# The shell variables defined here will be used by xmecca 
+# when it is run in batch mode (i.e. not interactive).
+
+ set apn          = 1                 # number of aerosol phases [0...99, default=0]
+ set gaseqnfile   = gas.eqn
+ set rplfile      = maforchem         # replacements and additional reactions
+ set wanted       = "Tr && (G || Aa) && \!Br && \!Hg"
+ set enthalpy     = n                 # activate enthalpy in kJ/mol?
+ set mcfct        = n                 # Monte-Carlo factor?
+ set diagtracfile =                   # diagnostic tracers?
+ set rxnrates     = n                 # calculate accumulated reaction rates?
+ set tag          = n                 # perform (isotope) tagging?
+ set kppoption    = k                 # k=kpp, 4=kp4, q=quit
+ set integr       = rosenbrock_posdef # integrator
+ set decomp       = n                 # remove indirect indexing
+ set latex        = y                 # latex list of reactions?
+ set graphviz     = n                 # graphviz plots?
+ set deltmpkp4    = y                 # delete temporary kp4 files?
+ set deltmp       = y                 # delete temporary xmecca files?
